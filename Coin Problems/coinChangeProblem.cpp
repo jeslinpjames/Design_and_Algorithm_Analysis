cdaee2cdbccf00d853594cdac801dae3534dc31d
jeslinpjames <jeslinpjames007@gmail.com>
2023-12-12 22:37:10 +0530

    Commiting changes

--- Coin Problems/coinChangeProblem.cpp
+++ Coin Problems/coinChangeProblem.cpp
@@ -6,14 +6,16 @@ private:
     int n;
     int *coins;
     int amount;
+    int* minCoins;
+    int INT_MAX_CUSTOM = 2147483647;  // Custom definition of INT_MAX
 
 public:
     CoinChange() : n(0), coins(nullptr), amount(0) {}
 
     void inputDenominationsAndAmount();
     int countWays();
+    int minCoinCount();
     ~CoinChange();
-
 };
 
 void CoinChange::inputDenominationsAndAmount() {
@@ -26,6 +28,11 @@ void CoinChange::inputDenominationsAndAmount() {
     }
     cout << "Enter the target amount: ";
     cin >> amount;
+    minCoins = new int[amount + 1];
+    for (int i = 0; i <= amount; i++) {
+        minCoins[i] = INT_MAX_CUSTOM;
+    }
+    minCoins[0] = 0;
 }
 
 int CoinChange::countWays() {
@@ -44,14 +51,32 @@ int CoinChange::countWays() {
     return dp[amount];
 }
 
+int CoinChange::minCoinCount() {
+    if (n <= 0)
+        return -1;
+
+    for (int i = 1; i <= amount; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i - coins[j] >= 0 && minCoins[i - coins[j]] != INT_MAX_CUSTOM) {
+                minCoins[i] = min(minCoins[i], minCoins[i - coins[j]] + 1);
+            }
+        }
+    }
+
+    return minCoins[amount];
+}
+
 CoinChange::~CoinChange() {
     delete[] coins;
+    delete[] minCoins;
 }
 
 int main() {
     CoinChange ob;
     ob.inputDenominationsAndAmount();
     int numWays = ob.countWays();
+    int minCoinCount = ob.minCoinCount();
     cout << "Number of ways to make change: " << numWays << endl;
+    cout << "Minimum number of coins: " << minCoinCount << endl;
     return 0;
 }
