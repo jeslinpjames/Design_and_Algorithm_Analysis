59d09601396a5b4806e0596500a87ee0a8c2ac56
jeslinpjames <jeslinpjames007@gmail.com>
2023-12-18 12:19:31 +0530

    cycle 2 and 1 complete

--- LAB_CYCLE_3/0-1Knapsack.cpp
+++ LAB_CYCLE_3/0-1Knapsack.cpp
@@ -1,47 +1,73 @@
-/* A Naive recursive implementation of 
-0-1 Knapsack problem */
-#include <bits/stdc++.h> 
-using namespace std; 
-
-// A utility function that returns 
-// maximum of two integers 
-int max(int a, int b) { return (a > b) ? a : b; } 
-
-// Returns the maximum value that 
-// can be put in a knapsack of capacity W 
-int knapSack(int W, int wt[], int val[], int n) 
-{ 
-
-	// Base Case 
-	if (n == 0 || W == 0) 
-		return 0; 
-
-	// If weight of the nth item is more 
-	// than Knapsack capacity W, then 
-	// this item cannot be included 
-	// in the optimal solution 
-	if (wt[n - 1] > W) 
-		return knapSack(W, wt, val, n - 1); 
-
-	// Return the maximum of two cases: 
-	// (1) nth item included 
-	// (2) not included 
-	else
-		return max( 
-			val[n - 1] 
-				+ knapSack(W - wt[n - 1], wt, val, n - 1), 
-			knapSack(W, wt, val, n - 1)); 
-} 
-
-// Driver code 
-int main() 
-{ 
-	int profit[] = { 60, 100, 120 }; 
-	int weight[] = { 10, 20, 30 }; 
-	int W = 50; 
-	int n = sizeof(profit) / sizeof(profit[0]); 
-	cout << knapSack(W, weight, profit, n); 
-	return 0; 
-} 
-
-// This code is contributed by rathbhupendra
+#include<iostream>
+using namespace std;
+class KnapsackProblem{
+	private:
+		int * weights;
+		int * values;
+		int capacity;
+		int items;
+	public:
+		void input(){
+			cout<<"Enter the number of items : ";
+			cin>> items;
+			weights = new int [items];
+			values = new int [items];
+			cout << "Enter the weights of the items: ";
+			for (int i = 0; i < items; ++i)
+			{
+				cin >> weights[i];
+			}
+			cout << "Enter the values of the items : ";
+			for (int i = 0; i < items; ++i)
+			{
+				cin >> values[i];
+			}
+			cout<<"Enter the capacity : ";
+			cin>>capacity;
+		}
+		void findSolution(){
+			int ** matrix = new int *[items+1];
+			for(int i =0;i<= items;i++){
+				matrix[i]= new int[capacity+1];
+			}
+			for (int i =0;i<=items;i++){
+				for(int j =0;j<=capacity;j++){
+					if((i==0)||(j==0)){
+						matrix[i][j] = 0;
+					}
+					else if (j-weights[i-1]<0){
+						matrix[i][j]=matrix[i-1][j];
+					}
+					else {
+						matrix[i][j] = max (matrix[i-1][j],(values[i-1]+matrix[i-1][j-weights[i-1]]));
+					}
+				}
+			}
+			cout<<"Max profit for the capacity is : "<<matrix[items][capacity]<<endl;
+			int selected [items];
+			int Count=0;
+			int i =items;
+			int j = capacity;
+			while(i>0 && j>0){
+				if (matrix[i][j]!=matrix[i-1][j]){
+					selected[Count++]=i;
+					j -= weights[i - 1];
+            		i--;
+				}
+				else{
+					i--;
+				}
+			}
+			cout<<"Weights of selected items : ";
+			for(int i= Count-1;i>=0;i--){
+				cout<<selected[i]+1<<" ";
+			}
+		}
+};
+int main(){
+	KnapsackProblem ob;
+	ob.input();
+	ob.findSolution();
+	cout<<endl;
+	return 0;
+}
--- LAB_CYCLE_3/LongestCommonSubsequence.cpp
+++ LAB_CYCLE_3/LongestCommonSubsequence.cpp
@@ -23,19 +23,75 @@ public:
         delete[] count;
     }
 
-    void LCS_Length(char* A, char* B, int m, int n) {
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (A[i - 1] == B[j - 1]) {
-                    count[i][j] = count[i - 1][j - 1] + 1;
-                } else if (count[i - 1][j] >= count[i][j - 1]) {
-                    count[i][j] = count[i - 1][j];
-                } else {
-                    count[i][j] = count[i][j - 1];
+    void printLCS(int row, int col, string text1, string text2, int **mat)
+    {
+        int length = mat[row][col];
+        char *lcs = new char[length + 1];
+
+        int i = row, j = col, index = length;
+        while (i > 0 && j > 0)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                lcs[index] = text1[i - 1];
+                i--;
+                j--;
+                index--;
+            }
+            else if (mat[i - 1][j] > mat[i][j - 1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        cout << "\nThe largest common subsequence is: ";
+        for (int k = 1; k <= length; k++)
+        {
+            cout << lcs[k];
+        }
+
+        delete[] lcs;
+    }
+
+    void lcs(int row, int col, string text1, string text2)
+    {
+        int **mat = new int *[row + 1];
+        for (int i = 0; i <= row; i++)
+        {
+            mat[i] = new int[col + 1];
+        }
+
+        for (int i = 0; i <= row; i++)
+        {
+            for (int j = 0; j <= col; j++)
+            {
+                if (i == 0 || j == 0)
+                {
+                    mat[i][j] = 0;
+                }
+                else if (text1[i - 1] == text2[j - 1])
+                {
+                    mat[i][j] = mat[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    mat[i][j] = max(mat[i - 1][j], mat[i][j - 1]);
                 }
             }
         }
-        cout << count[m][n] << endl;
+        cout << "\n"<< "The largest common subsequence length is: " << mat[row][col];
+        printLCS(row, col, text1, text2, mat);
+
+        // Deallocate memory
+        for (int i = 0; i <= row; i++)
+        {
+            delete[] mat[i];
+        }
+        delete[] mat;
     }
 };
 
@@ -49,7 +105,6 @@ int main() {
     for (int i = 0; i < x; i++) {
         cin >> A[i];
     }
-
     cout << "Enter length of second sequence: ";
     cin >> y;
     char* B = new char[y];
@@ -59,9 +114,10 @@ int main() {
     }
 
     LongestCommonSubsequence lcs(x, y);
-    lcs.LCS_Length(A, B, x, y);
+    lcs.lcs(x, y,A,B);
 
     delete[] A;
     delete[] B;
+    cout<<endl;
     return 0;
 }
